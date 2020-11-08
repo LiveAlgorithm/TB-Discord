080f634a8a0d590023e686a76ae783661849c003
LiveAlgorithm <73899628+LiveAlgorithm@users.noreply.github.com>
2020-11-08 09:27:48 -0300

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 app.get("/", (request, response) => {
   const ping = new Date();
   ping.setHours(ping.getHours() - 3);
-  console.log(Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()});
+  console.log(Ping recebido às 
+  ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()});
   response.sendStatus(200);
 });
 app.listen(process.env.PORT); // Recebe solicitações que o deixa online
