bad87f113dba5843ed8dc5ee49afe6409cd7fe19
LiveAlgorithm <73899628+LiveAlgorithm@users.noreply.github.com>
2020-11-08 12:20:39 -0300

    Update diz.js

--- commands/diz.js
+++ commands/diz.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   const sayMessage = args.join(" ");
-  message.delete().catch(O_o => {});
+  message.delete().catch(O_o => {}); // opcional para funções em roles
   message.channel.send(sayMessage);
 };
